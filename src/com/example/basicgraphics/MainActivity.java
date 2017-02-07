c4c10ad34d2d5bcdd932be27c6d2e2216a3a91a9
ITLAB <ITLAB@IT216>
2017-02-07 14:01:21 +0530

    seventhcommit

--- src/com/example/basicgraphics/MainActivity.java
+++ src/com/example/basicgraphics/MainActivity.java
@@ -8,65 +8,59 @@ import android.graphics.Paint;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.View;
- 
-public class MainActivity extends Activity {
- 
+
+public class MainActivity extends Activity 
+{
+
 	DemoView demo;
- 
+
 	@Override
-	public void onCreate(Bundle savedInstanceState) {
+	public void onCreate(Bundle savedInstanceState) 
+	{
 		super.onCreate(savedInstanceState);
 		demo = new DemoView(this);
 		setContentView(demo);
 	}
- 
-	private class DemoView extends View {
-		public DemoView(Context mcontext) {
+
+	private class DemoView extends View
+	{
+		public DemoView(Context mcontext)
+		{
 			super(mcontext);
 		}
- 
+
 		@Override
-		protected void onDraw(Canvas canvas) {
+		protected void onDraw(Canvas canvas)
+		{
 			super.onDraw(canvas);
- 
+
 			// custom drawing code here
 			Paint paint = new Paint();
 			paint.setStyle(Paint.Style.FILL);
- 
+
 			// make the entire canvas white
-			paint.setColor(Color.CYAN);
+			paint.setColor(Color.WHITE);
 			canvas.drawPaint(paint);
- 
+
 			// draw blue circle with anti aliasing turned off
 			paint.setAntiAlias(false);
-			paint.setColor(Color.MAGENTA);
+			paint.setColor(Color.BLUE);
 			canvas.drawCircle(20, 20, 15, paint);
- 
-			// draw green circle with anti aliasing turned on
-			paint.setAntiAlias(true);
-			paint.setColor(Color.GRAY);
-			canvas.drawCircle(60, 20, 15, paint);
- 
-			// draw red rectangle with anti aliasing turned off
+
+			// draw black rectangle with anti aliasing turned off
 			paint.setAntiAlias(false);
-			paint.setColor(Color.RED);
+			paint.setColor(Color.BLACK);
 			canvas.drawRect(100, 5, 200, 30, paint);
- 
-			// draw the rotated text
-			canvas.rotate(-40);
- 
-			
- 
-			// undo the rotate
-			canvas.restore();
+
 		}
 	}
- 
+
 	@Override
-	public boolean onCreateOptionsMenu(Menu menu) {
+	public boolean onCreateOptionsMenu(Menu menu) 
+	{
 		// Inflate the menu; this adds items to the action bar if it is present.
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}
- 
+
 }
