8a197579ee7ad36b7812ab6ccce20766e68759e2
ITLAB <ITLAB@IT216>
2017-02-07 13:36:59 +0530

    fourthcommit

--- src/com/example/basicgraphics/MainActivity.java
+++ src/com/example/basicgraphics/MainActivity.java
@@ -24,4 +24,42 @@ public class MainActivity extends Activity {
 		public DemoView(Context mcontext) {
 			super(mcontext);
 		}
+		@Override
+		protected void onDraw(Canvas canvas) {
+			super.onDraw(canvas);
+ 
+			// custom drawing code here
+			Paint paint = new Paint();
+			paint.setStyle(Paint.Style.FILL);
+ 
+			// make the entire canvas white
+			paint.setColor(Color.CYAN);
+			canvas.drawPaint(paint);
+ 
+			// draw blue circle with anti aliasing turned off
+			paint.setAntiAlias(false);
+			paint.setColor(Color.MAGENTA);
+			canvas.drawCircle(20, 20, 15, paint);
+ 
+			// draw green circle with anti aliasing turned on
+			paint.setAntiAlias(true);
+			paint.setColor(Color.GRAY);
+			canvas.drawCircle(60, 20, 15, paint);
+ 
+			// draw red rectangle with anti aliasing turned off
+			paint.setAntiAlias(false);
+			paint.setColor(Color.RED);
+			canvas.drawRect(100, 5, 200, 30, paint);
+ 
+			// draw the rotated text
+			canvas.rotate(-40);
+ 
+			paint.setStyle(Paint.Style.FILL);
+			canvas.drawText("Graphics Rotation", 45, 185, paint);
+ 
+			// undo the rotate
+			canvas.restore();
+		}
+	}
+		
  
